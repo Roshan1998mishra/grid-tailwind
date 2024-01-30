d6055a5a4bf5650c54cebc714f6ebd19f7f6788e
Roshan1998mishra <mishraroshan378@gmail.com>
2024-01-30 13:03:11 +0530

    fisrt

---
+++ Components/Grid.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <>
+    <h1 className='text-center'>Grid using Tailwind Css</h1>
+    <div className='p-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4'>
+        <div className='bg-red-500'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+            <div className='bg-red-500'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+
+            <div className='bg-green-600'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. 
+            For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.</div>
+
+    </div>
+    </>
+  )
+}
+
+export default Grid
